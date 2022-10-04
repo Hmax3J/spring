b8a84be00a526fd3d475377c4fdde21383c3b5f2
Hmax3J <Hmax3J@gmail.com>
2022-10-04 14:30:10 +0900

    2022.10.4

--- spring.web/src/main/java/com/my/spring/web/ch02/ex01/User.java
+++ spring.web/src/main/java/com/my/spring/web/ch02/ex01/User.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data // setter getter 를 만들어라 라는 뜻이다. 동시에 만든다.
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // 기본 생성자를 만든다.
+@AllArgsConstructor // 파라미터를 가진 필드로 만든다.
 public class User {
 	private String userName;
 	private int age;
